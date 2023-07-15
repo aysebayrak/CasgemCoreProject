7d9814886a917cb56d4aec785c7678ccadb97fe9
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2023-07-15 18:07:48 +0300

    bussineslayer

---
+++ PizzaPanBussinesLayer/Concrete/ReferenceManager.cs
@@ -0,0 +1,47 @@
+﻿using PizzaPan.BussinesLayer.Abstract;
+using PizzaPan.DataAccessLayer.Abstract;
+using PizzaPan.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPan.BussinesLayer.Concrete
+{
+    public class ReferenceManager : IReferenceService
+    {
+
+        private readonly IReferenceDal _referenceDal;
+
+        public ReferenceManager(IReferenceDal referenceDal)
+        {
+            _referenceDal = referenceDal;   
+        }
+
+        public void TDelete(Reference t)
+        {
+           _referenceDal.Delete(t); 
+        }
+
+        public Reference TGetById(int id)
+        {
+            return _referenceDal.GetById(id);   
+        }
+
+        public List<Reference> TGetList()
+        {
+            return _referenceDal.GetList();
+        }
+
+        public void TInsert(Reference t)
+        {
+            _referenceDal.Insert(t);
+        }
+
+        public void TUpdate(Reference t)
+        {
+            _referenceDal.Update(t);
+        }
+    }
+}
