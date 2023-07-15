d59d92e66432831bfe49c6487db122078e924aa9
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2023-07-15 17:25:20 +0300

    customers

---
+++ PizzaPan.PresentationLayer/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPan.BussinesLayer.Abstract;
+
+namespace PizzaPan.PresentationLayer.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        private readonly ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService; 
+
+        }
+        public IActionResult Index()
+        {
+            var values = _customerService.TGetList();
+            return View(values);
+        }
+    }
+}
