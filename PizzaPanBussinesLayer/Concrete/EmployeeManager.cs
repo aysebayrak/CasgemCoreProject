f36a9eb3ace97117304c50cd0a49711bc9dc5650
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2023-07-16 19:37:26 +0300

    employee repository

---
+++ PizzaPanBussinesLayer/Concrete/EmployeeManager.cs
@@ -0,0 +1,46 @@
+﻿using PizzaPan.BussinesLayer.Abstract;
+using PizzaPan.DataAccessLayer.Abstract;
+using PizzaPan.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPan.BussinesLayer.Concrete
+{
+    public class EmployeeManager : IEmployeeService
+    {
+
+        private readonly IEmployeeDal _employeeDal;
+        public EmployeeManager(IEmployeeDal employeeDal)
+        {
+            _employeeDal = employeeDal;
+        }
+
+        public void TDelete(Employee t)
+        {
+           _employeeDal.Delete(t);
+        }
+
+        public Employee TGetById(int id)
+        {
+           return _employeeDal.GetById(id); 
+        }
+
+        public List<Employee> TGetList()
+        {
+            return _employeeDal.GetList();
+        }
+
+        public void TInsert(Employee t)
+        {
+           _employeeDal.Insert(t);
+        }
+
+        public void TUpdate(Employee t)
+        {
+            _employeeDal.Update(t);
+        }
+    }
+}
